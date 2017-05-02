2c542c2d65bb0e2a76212cb9585ddfb52d386056
Scott Hasbrouck <scott@lob.com>
2017-05-02 13:49:24 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+/**
+  * @index.js
+  * Lob.com Customized Postcard Demo Node Program
+
+  * @author Scott Hasbrouck <scott@lob.com>
+  * @copyright Lob, Inc. 2017
+*/
+
+"use strict";
+
+//  ========== 3rd Party Dependencies ==========
+// Command line prompt I/O module
+var prompt = require('prompt');
+
+// Module for generating coupon codes
+var cc = require('coupon-code');
+
+// Node's file I/O module
+var fs = require('fs');
+// ========== End 3rd Party dependencies ==========
+
+
+// ========== Utility module for interacting with Google Maps API ==========
+var utils = require('./utils.js');
+
+
+// ========== Import configuration as JSON ==========
+// `config.json` is where we specify:
+// - API keys for Lob and Google
+// - Templates for front and back of postcard
+// - Postcard size and description
+// - From address and name
+// - Google map dimensions
+var CONFIG = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+
+// Lob's Node module
+var Lob = require('lob')(CONFIG.lob_api_key);
+
+// format origin address from CONFIG file
+var bobSandwichesAddress = {
+	name: CONFIG.from_name,
+	address_line1: CONFIG.from_address1,
+	address_city: CONFIG.from_address_city,
+	address_state: CONFIG.from_address_state,
+	address_zip: CONFIG.from_address_zip
+};
+
+
+// Array of the inputs to collect from the prompt
+var inputs = [
+	'name',
+	'address_line1',
+	'address_line2',
+	'address_city',
+	'address_state',
+	'address_zip'
+];
+
+// Initialize the prompt to collect the inputs required to send a postcard to a customer
+prompt.start();
+prompt.get(inputs, function(err, customerAddress) {
+
+	// Invoke the utility function to generate the google maps
+	// directions between the customer to Bob's Sandwiches
+	utils.getDirectionsPaths(customerAddress, bobSandwichesAddress)
+
+	// Generate the static image google maps URL and URL shortner
+	// (note: we must use a URL shortner, because we'll be passing the google maps URL
+	// to Lob's API as a data parameter so we can put the image on the postcard.
+	// Lob's API has a 500 character limit on all values passed as data parameters.)
+	.then(utils.getMap)
+
+	// Finally, create the Postcard using the Lob Node Library
+	/// passing the generated short URL of the static google map image as a data parameter
+	.then(function(short_map_url) {
+
+		// All methods in Lob's Node Library return a promise
+		return Lob.postcards.create({
+		 	description: CONFIG.postcard_description,
+		 	to: customerAddress,
+		 	from: bobSandwichesAddress,
+		 	back: CONFIG.template_back,
+		 	front: CONFIG.template_front,
+		 	mail_type: 'usps_first_class',
+		 	size: CONFIG.postcard_size,
+
+		 	// Data object that will be mapped to the merge variables in the Postcard template
+		 	data: {
+		 		// genrates a coupon code using the `coupon-code` module.
+		 		// in a real-world implementation, this code would likely
+		 		// be saved in your customer database
+		 		code: cc.generate({ parts: 2 }),
+		 		name: customerAddress.name,
+
+		 		// the short URL for the static googe maps image
+		 		map_url: short_map_url.data.id
+		 	}
+		 });
+	})
+	.then(function() {
+		return console.log('Postcard sent to ' + customerAddress.name + '!');
+	})
+	.catch(function(err) { console.error(err.message); });
+});
---
+++ utils.js
@@ -0,0 +1,74 @@
+
+//  ========== 3rd Party Dependencies ==========
+// Node library for making HTTP requests with Promise support
+var axios = require('axios');
+
+// Library for URL encoding strings
+var urlencode = require('urlencode');
+// ========== End 3rd Party dependencies ==========
+
+// Node's file I/O module
+var fs = require('fs');
+
+// Import configuration as JSON
+var CONFIG = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+var utils = {
+	/**
+	  * Converts an address object to a string for google maps
+	  * @param {Object} addressObject - Lob formatted address object
+	  * @returns {String} String of address
+	  */
+	addressToString: function(addressObject) {
+		var oneLineAddress = '';
+		for (var key in addressObject) {
+			if (key !== 'name' && addressObject[key] && addressObject[key].length > 0) {
+				oneLineAddress = oneLineAddress + ' ' + addressObject[key];
+			}
+		}
+		return oneLineAddress;
+	},
+
+	/**
+	  * @param {Object} origin - Lob formatted address object of origin for driving directions
+	  * @param {Object} destination - Lob formatted address object of destination for driving directions
+	  * @returns {Promise} result - resolves with urlencoded {String} driving direction path
+	  */
+	getDirectionsPath: function(origin, destination) {
+		return axios.get('https://maps.googleapis.com/maps/api/directions/json?origin='
+			+ utils.addressToString(origin)
+			+ '&destination='
+			+ utils.addressToString(destination)
+			+ '&key='
+			+ CONFIG.google_maps_api_key)
+		.then(function(result) {
+			if (result.data.routes.length > 0) {
+				return urlencode(result.data.routes[0].overview_polyline.points);
+			}
+		});
+	},
+
+	/**
+	  * @param {String} path - URLEncoded string of a polygon path representing driving directions
+	  * @returns {Promise} result - resolves with an {Object} containing a Google generated
+	  *   short URL of the static google maps image of driving directions
+	  */
+	getMap: function(path) {
+		var map_url = 'https://maps.googleapis.com/maps/api/staticmap?size='
+			+ CONFIG.google_map_width
+			+ 'x'
+			+ CONFIG.google_map_height
+			+ '&path=enc%3A'
+			+ path
+			+ '&key='
+			+ CONFIG.google_maps_api_key;
+
+		return axios.post('https://www.googleapis.com/urlshortener/v1/url'
+			+ '?key='
+			+ CONFIG.google_short_url_api_key, {
+		 		longUrl: map_url
+			});
+	}
+};
+
+module.exports = utils;
